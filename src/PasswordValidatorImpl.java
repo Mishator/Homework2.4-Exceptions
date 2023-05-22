0e6b8924b85aec29ccf1ab73c8d63787e6f7f052
Mishator <voyteh89@mail.ru>
2023-05-22 10:33:07 +0300

    Сделал правку кода.

--- src/PasswordValidatorImpl.java
+++ src/PasswordValidatorImpl.java
@@ -4,46 +4,63 @@ public class PasswordValidatorImpl implements PasswordValidator {
 
     @Override
     public boolean validatePassword(String login, String password, String confirmPassword) {
-        throws WrongLoginException, WrongPasswordException {
-            boolean result = false;
-
-            if (!login.matches(regex)) {
+        boolean result = false;
+        if (!login.matches(regex)) {
+            try {
                 throw new WrongLoginException("Логин содержит недопустимые символы");
-            } else if (login.length() >= lengthMax) {
+            } catch (WrongLoginException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (login.length() >= lengthMax) {
+            try {
                 throw new WrongLoginException("Логин слишком длинный");
+            } catch (WrongLoginException e) {
+                throw new RuntimeException(e);
+            }
 
-            } else if (!password.matches(regex) || !confirmPassword.matches(regex)) {
+        } else if (!password.matches(regex) || !confirmPassword.matches(regex)) {
+            try {
                 throw new WrongPasswordException("Пароль содержит недопустимые символы");
-            } else if (password.length() >= lengthMax || confirmPassword.length() >= lengthMax) {
+            } catch (WrongPasswordException e) {
+                throw new RuntimeException(e);
+            }
+        } else if (password.length() >= lengthMax || confirmPassword.length() >= lengthMax) {
+            try {
                 throw new WrongPasswordException("Пароль слишком длинный");
+            } catch (WrongPasswordException e) {
+                throw new RuntimeException(e);
+            }
 
-            } else if (!password.equals(confirmPassword)) {
+        } else if (!password.equals(confirmPassword)) {
+            try {
                 throw new WrongPasswordException("Пароль и подтверждение не совпадают");
-
-            } else {
-                result = true;
-                printResult(login, password, confirmPassword, result);
+            } catch (WrongPasswordException e) {
+                throw new RuntimeException(e);
             }
 
-            return result;
+        } else {
+            result = true;
+            printResult(login, password, confirmPassword, result);
         }
 
-        private void printResult (String login, String password, String confirmPassword,boolean result){
-            System.out.println("This set: ");
-            System.out.println(
-                    login + " -> login ok\n"
-                            + password + " -> password ok\n"
-                            + confirmPassword + " -> confirmPassword ok"
-            );
-
-            if (result) {
-                System.out.println(result + " -> passwords are equal\n");
-            } else {
-                System.out.println(result + " -> passwords are not equal\n");
-            }
-        }
+        return result;
     }
 
     private void printResult(String login, String password, String confirmPassword, boolean result) {
+        System.out.println("This set: ");
+        System.out.println(
+                login + " -> login ok\n"
+                        + password + " -> password ok\n"
+                        + confirmPassword + " -> confirmPassword ok"
+        );
+
+        if (result) {
+            System.out.println(result + " -> passwords are equal\n");
+        } else {
+            System.out.println(result + " -> passwords are not equal\n");
+        }
     }
 }
+
+
+
